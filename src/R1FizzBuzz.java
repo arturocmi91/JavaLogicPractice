5d2455c9f87db0fb1fae882989b3ecb0cbb7d931
Arturo Mendoza <arturocmi91@gmail.com>
2024-04-22 14:11:53 +0200

    fizzbuzz java

---
+++ src/R1FizzBuzz.java
@@ -0,0 +1,18 @@
+public class R1FizzBuzz {
+    public static void main(String[] args){
+        for(int i=0; i<=100;i++){
+            int mult3= i % 3;
+            int mult5= i % 5;
+            if(mult3==0 && mult5==0){
+                System.out.println("FizzBuzz "+i);
+            } else if (mult3==0) {
+                System.out.println("Fizz "+i);
+            }else if (mult5==0){
+                System.out.println("Buzz "+i);
+            }else {
+                System.out.println("none"+ i);
+            }
+        }
+    }
+
+}
